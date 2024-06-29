425b10ddb81938f630cfb4f2977522de74e5f44e
clw <2682855462@qq.com>
2024-06-29 13:32:14 +0800

    完成流式压缩数据点

--- test/main.cpp
+++ test/main.cpp
@@ -32,47 +32,60 @@ void test_hf()
     tsdb_hf_cpp::point point("point1", 42.42, 1622547800000LL);
     tsdb_hf_cpp::tsdb_entry tsdb_entry;
     std::vector<tsdb_hf_cpp::point> points;
+    std::string timestampStr = "2024-02-19 12:34:56";
+    // 日期字符串转纳秒
+    long long nanoseconds = stringToNanoseconds(timestampStr);
 
-    tsdb_entry.insert_point(point);
-
-    tsdb_entry.extract_points("timestamps.zst", "values.zst", points);
-
-    for (auto point : points) {
-        cout << point.name_ << "," << point.nanoseconds_ << "," << point.value_ << endl;
+    // 数据点数
+    int point_count = 100000;
+    for (int i = 0; i < point_count; i++) {
+        nanoseconds += 1000000;
+        // 构造一个点，点名为point1, 值为i，时间戳为nanoseconds
+        tsdb_hf_cpp::point point("point1", i, nanoseconds);
+        points.push_back(point);
     }
+
+    tsdb_entry.insert_points(points);
+    auto extract_points = tsdb_entry.extract_points("timestamps.zst", "values.zst");
+    
+    for (auto p : extract_points) {
+        cout << p.name_ << "," << p.nanoseconds_ << "," << p.value_ << endl;
+    }
+
 }
+
 
 int main(int argc, char const* argv[])
 {
-    // std::string timestampStr = "2024-02-19 12:34:56";
-    // // 日期字符串转纳秒
-    // long long nanoseconds = stringToNanoseconds(timestampStr);
+     using namespace tsdb_hf_cpp;
+     std::string timestampStr = "2024-02-19 12:34:56";
+     // 日期字符串转纳秒
+     long long nanoseconds = stringToNanoseconds(timestampStr);
 
-    // // 本地或远程时序数据库的主机名和端口
-    // tsdb_cpp::tsdb_entry tsdb_entry("127.0.0.1", 8191);
-    // std::vector<tsdb_cpp::point> points;
+     // 本地或远程时序数据库的主机名和端口
+     tsdb_entry tsdb_entry("127.0.0.1", 8191);
+     std::vector<point> points;
 
-    // // 数据点数
-    // int point_count = 1000000;
-    // for (int i = 0; i < point_count; i++) {
-    //     nanoseconds += 1000000;
-    //     // 构造一个点，点名为point1, 值为i，时间戳为nanoseconds
-    //     tsdb_cpp::point point("point1", i, nanoseconds);
-    //     points.push_back(point);
-    // }
+     // 数据点数
+     int point_count = 1000000;
+     for (int i = 0; i < point_count; i++) {
+         nanoseconds += 1000000;
+         // 构造一个点，点名为point1, 值为i，时间戳为nanoseconds
+         point point("point1", i, nanoseconds);
+         points.push_back(point);
+     }
 
-    // // 记录插入时序数据库时间，获取插入操作开始时间点
-    // std::clock_t start = std::clock();
-    // int ret = tsdb_entry.insert_points(points);
-    // // 获取插入操作结束时间点
-    // std::clock_t end = std::clock();
+     // 记录插入时序数据库时间，获取插入操作开始时间点
+     std::clock_t start = std::clock();
+     int ret = tsdb_entry.insert_points(points);
+     // 获取插入操作结束时间点
+     std::clock_t end = std::clock();
 
-    // tsdb_entry.close();
-    // // 计算插入操作耗时时间（以毫秒为单位）
-    // double duration = static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000);
-    // // 输出耗时时间
-    // std::cout << "Operation took " << duration << " microseconds." << std::endl;
+     tsdb_entry.close();
+     // 计算插入操作耗时时间（以毫秒为单位）
+     double duration = static_cast<double>(end - start) / (CLOCKS_PER_SEC / 1000);
+     // 输出耗时时间
+     std::cout << "Operation took " << duration << " microseconds." << std::endl;
 
-    // cout << ret << endl;
-    test_hf();
+     cout << ret << endl;
 }
--- tsdb_hf.hpp
+++ tsdb_hf.hpp
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <sched.h>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -40,10 +41,10 @@ using namespace std;
 namespace tsdb_hf_cpp {
 
 struct point {
-    const std::string& name_;
+    const string& name_;
     double value_;
     long long nanoseconds_;
-    point(const std::string& name, double value, long long nanoseconds)
+    point(const string& name, double value, long long nanoseconds)
         : name_(name)
         , value_(value)
         , nanoseconds_(nanoseconds)
@@ -57,7 +58,7 @@ struct tsdb_entry {
     {
     }
 
-    tsdb_entry(const std::string& host, int port)
+    tsdb_entry(const string& host, int port)
         : host_(host)
         , port_(port)
     {
@@ -73,64 +74,178 @@ struct tsdb_entry {
     }
 
     // 将数据压缩并写入文件的辅助函数
-    bool compressAndWriteToFile(const std::string& filename, const void* data, size_t dataSize)
+    bool compressAndWriteToFile(const string& filename, const void* data, size_t dataSize)
     {
         // 估算压缩后的最大缓冲区大小
         size_t const cBuffSize = ZSTD_compressBound(dataSize);
-        std::vector<char> cBuff(cBuffSize);
+        vector<char> cBuff(cBuffSize);
 
         // 压缩数据
         size_t const cSize = ZSTD_compress(cBuff.data(), cBuffSize, data, dataSize, 1);
         if (ZSTD_isError(cSize)) {
-            std::cerr << "ZSTD_compress error: " << ZSTD_getErrorName(cSize) << std::endl;
+            cerr << "ZSTD_compress error: " << ZSTD_getErrorName(cSize) << endl;
             return false;
         }
 
         // 将压缩后的数据写入文件
-        std::ofstream outFile(filename, std::ios::trunc | std::ios::binary);
+        ofstream outFile(filename, ios::trunc | ios::binary);
         if (!outFile) {
-            std::cerr << "Failed to open file " << filename << std::endl;
+            cerr << "Failed to open file " << filename << endl;
             return false;
         }
         outFile.write(cBuff.data(), cSize);
         return true;
     }
 
+    bool streamCompressToFile(const string& filename, const vector<char>& input)
+    {
+        ofstream outFile(filename, ios::binary);
+        if (!outFile) {
+            cerr << "Failed to open file " << filename << endl;
+            return false;
+        }
+
+        // 创建压缩上下文
+        ZSTD_CCtx* cctx = ZSTD_createCCtx();
+        if (!cctx) {
+            cerr << "Failed to create ZSTD_CCtx" << endl;
+            return false;
+        }
+        // 配置压缩参数
+        ZSTD_initCStream(cctx, ZSTD_c_compressionLevel);
+        ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, 3);
+
+        // 创建输出缓冲区
+        const size_t outBuffSize = ZSTD_CStreamOutSize(); // 获取推荐的缓冲区大小
+        vector<char> output(outBuffSize);
+
+        ZSTD_inBuffer inBuff = { input.data(), input.size(), 0 };
+
+        /**
+         * @brief size_t ZSTD_compressStream2(ZSTD_CCtx* cctx, ZSTD_outBuffer* output, ZSTD_inBuffer* input
+                                              , ZSTD_EndDirective endOp)
+         * @description zstd的流式压缩API。
+         * zstd在单次流式压缩时将所有数据压缩为一个frame，frame由header和不同的固定大小的block组成，因此理论上可以实现无限大小的数据压缩。
+         * 但由于压缩缓冲区ZSTD_outBuffer的大小有限，可能会出现缓冲区已满但没有形成一个完整大小block的情况，
+         * 因此我们需要根据ZSTD_compressStream2()的返回结果,传入不同endOp，执行不同的操作。
+         * @return remaining == 0 缓冲区有足够空间，endOp = ZSTD_e_continue继续压缩。
+         * @return remaining > 0 缓冲区需要额外的remaining大小空间，endOp = ZSTD_e_flush，立即将当前缓冲区刷入frame，
+                   不足一个block的数据也形成一个block。
+                   若压缩结束，令endOp = ZSTD_e_end，会执行与ZSTD_e_flush相同的操作，并在frame尾部写入结束符。
+         * @return remaining < 0 压缩出错
+         *
+         */
+        ZSTD_EndDirective endOp = ZSTD_e_continue;
+        ZSTD_outBuffer outBuff = { output.data(), outBuffSize, 0 };
+        while (inBuff.pos < inBuff.size) {
+            size_t const remaining = ZSTD_compressStream2(cctx, &outBuff, &inBuff, endOp);
+            if (ZSTD_isError(remaining)) {
+                cerr << "ZSTD_compressStream error: " << ZSTD_getErrorName(remaining) << endl;
+                ZSTD_freeCCtx(cctx);
+                return false;
+            }
+            // 若缓冲区满，立即将当前缓冲区刷入，并重置缓冲区大小
+            if (remaining > 0) {
+                endOp = ZSTD_e_flush;
+                outFile.write(output.data(), outBuff.size);
+                outBuff.pos = 0;
+            }
+        }
+
+        // 在frame尾部写入结束符，结束流压缩
+        size_t const remaining = ZSTD_compressStream2(cctx, &outBuff, &inBuff, ZSTD_e_end);
+        if (ZSTD_isError(remaining)) {
+            cerr << "ZSTD_endStream error: " << ZSTD_getErrorName(remaining) << endl;
+            ZSTD_freeCCtx(cctx);
+            return false;
+        }
+
+        outFile.write(output.data(), outBuff.pos);
+
+        // 释放上下文
+        ZSTD_freeCCtx(cctx);
+        return true;
+    }
+
+    vector<char> streamDecompressFromFile(const string& filename)
+    {
+        vector<char> output;
+        ifstream inFile(filename, ios::binary);
+        if (!inFile) {
+            cerr << "Failed to open file " << filename << endl;
+            return output;
+        }
+
+        ZSTD_DCtx* dctx = ZSTD_createDCtx();
+        if (!dctx) {
+            cerr << "Failed to create ZSTD_DCtx" << endl;
+            return output;
+        }
+        ZSTD_initDStream(dctx);
+
+        size_t const buffInSize = ZSTD_DStreamInSize();
+        vector<char> input(buffInSize);
+
+        size_t const buffOutSize = ZSTD_DStreamOutSize();
+        output.resize(buffOutSize);
+        ZSTD_outBuffer outBuff = { output.data(), buffOutSize, 0 };
+        
+        while (inFile) {
+            // 创建输入缓冲区
+            inFile.read(input.data(), buffInSize);
+            const size_t hasRead = inFile.gcount();
+            ZSTD_inBuffer inBuff = { input.data(), hasRead, 0 };
+            
+            // todo: 由于压缩数据过多，单次写入缓冲区不能完成解压缩的逻辑
+            while (inBuff.pos < inBuff.size) {
+                const size_t remaining = ZSTD_decompressStream(dctx, &outBuff, &inBuff);
+                if (ZSTD_isError(remaining)) {
+                    cerr << "ZSTD_decompressStream error: " << ZSTD_getErrorName(remaining) << endl;
+                    ZSTD_freeDCtx(dctx);
+                    return output;
+                }
+            }
+        }
+        output.resize(outBuff.pos);
+        ZSTD_freeDCtx(dctx);
+        return output;
+    }
+
     // 解压缩数据并读取到缓冲区的辅助函数
-    size_t decompressFromFile(const std::string& filename, char* const decompressedData)
+    size_t decompressFromFile(const string& filename, char* const decompressedData)
     {
         // 打开压缩文件
-        std::ifstream inFile(filename, std::ios::binary | std::ios::ate);
+        ifstream inFile(filename, ios::binary | ios::ate);
         if (!inFile) {
-            std::cerr << "Failed to open file " << filename << std::endl;
+            cerr << "Failed to open file " << filename << endl;
             return false;
         }
 
         // 获取文件大小
-        std::streamsize compressedSize = inFile.tellg();
-        inFile.seekg(0, std::ios::beg);
+        streamsize compressedSize = inFile.tellg();
+        inFile.seekg(0, ios::beg);
 
         // 读取压缩数据
         char* const compressedData = new char[compressedSize];
         if (!inFile.read(compressedData, compressedSize)) {
-            std::cerr << "Failed to read file " << filename << std::endl;
+            cerr << "Failed to read file " << filename << endl;
             return false;
         }
 
         // 获取解压后数据的最大尺寸
         unsigned long long const decompressedBound = ZSTD_getFrameContentSize(compressedData, compressedSize);
         if (decompressedBound == ZSTD_CONTENTSIZE_ERROR) {
-            std::cerr << "Not compressed by zstd" << std::endl;
+            cerr << "Not compressed by zstd" << endl;
             return false;
         } else if (decompressedBound == ZSTD_CONTENTSIZE_UNKNOWN) {
-            std::cerr << "Original size unknown" << std::endl;
+            cerr << "Original size unknown" << endl;
             return false;
         }
 
         // 解压缩数据
         size_t const dSize = ZSTD_decompress(decompressedData, 1600, compressedData, compressedSize);
         if (ZSTD_isError(dSize)) {
-            std::cerr << "ZSTD_decompress error: " << ZSTD_getErrorName(dSize) << std::endl;
+            cerr << "ZSTD_decompress error: " << ZSTD_getErrorName(dSize) << endl;
             return false;
         }
 
@@ -143,61 +258,71 @@ struct tsdb_entry {
         ::close(sock);
     }
 
-    inline int insert_points(const std::vector<point>& points)
+    template <typename T>
+    vector<char> vec2Bytes(const vector<T>& input)
     {
+        // 创建一个  vector<char>，其大小为输入向量大小乘以每个元素的字节大小
+        vector<char> output(input.size() * sizeof(T));
+
+        // 使用 memcpy 将数据从输入向量复制到输出向量中
+        memcpy(output.data(), input.data(), input.size() * sizeof(T));
+
+        return output;
     }
 
-    inline int insert_point(const point& point)
+    template <typename T>
+    vector<T> bytes2Vec(const vector<char>& bytes)
     {
-        // 压缩并写入时间戳
-        if (!compressAndWriteToFile("timestamps.zst", &point.nanoseconds_, sizeof(point.nanoseconds_))) {
-            return -1;
+        vector<T> output(bytes.size() / sizeof(T));
+        memcpy(output.data(), bytes.data(), bytes.size());
+        return output;
+    }
+
+    inline int insert_points(const vector<point>& points)
+    {
+        vector<long long> timestamps;
+        vector<double> values;
+        for (auto& p : points) {
+            timestamps.push_back(p.nanoseconds_);
+            values.push_back(p.value_);
         }
 
-        // 压缩并写入值
-        if (!compressAndWriteToFile("values.zst", &point.value_, sizeof(point.value_))) {
-            return -1;
-        }
+        auto timestampsInput = vec2Bytes(timestamps);
+        auto valuesInput = vec2Bytes(values);
+        streamCompressToFile("timestamps.zst", timestampsInput);
+        streamCompressToFile("values.zst", valuesInput);
 
         return 0;
     }
 
-    bool extract_points(const std::string& timestampsFile, const std::string& valuesFile, std::vector<point>& points)
+    vector<point> extract_points(const string& timestampsFile, const string& valuesFile)
     {
+        vector<point> points;
+        auto timestampsStream = streamDecompressFromFile("timestamps.zst");
+        auto valuesStream = streamDecompressFromFile("values.zst");
+        auto timestamps = bytes2Vec<long long>(timestampsStream);
+        auto values = bytes2Vec<double>(valuesStream);
 
-        char decompressedTimestamps[6400] = { 0 };
-        char decompressedValues[6400] = { 0 };
-
-        size_t timestampsCnt = decompressFromFile(timestampsFile, decompressedTimestamps) / sizeof(long long);
-        size_t valuesCnt = decompressFromFile(valuesFile, decompressedValues) / sizeof(double);
-
-        if (timestampsCnt != valuesCnt) {
-            std::cerr << "Mismatched sizes of decompressed timestamps and values" << std::endl;
-            return false;
+        if (timestamps.size() != values.size()) {
+            cerr << "Mismatched sizes of decompressed timestamps and values" << endl;
+            return points;
         }
 
-        // 组装点数据
-        points.clear();
-        points.reserve(valuesCnt);
-        for (size_t i = 0; i < valuesCnt; ++i) {
-            long long timestamp;
-            double value;
-            std::memcpy(&timestamp, decompressedTimestamps + i * sizeof(long long), sizeof(long long));
-            std::memcpy(&value, decompressedValues + i * sizeof(double), sizeof(double));
-            points.emplace_back(tsdb_hf_cpp::point("point", value, timestamp));
+        const auto name = new string("point");
+        for (size_t i = 0; i < timestamps.size(); i++) {
+            point p(*name, values[i], timestamps[i]);
+            points.push_back(p);
         }
-
-        return true;
+        return points;
     }
 
 protected:
     int sock;
     struct sockaddr_in addr;
-    const std::string& host_;
+    const string& host_;
     int port_;
     int points_count_per_package = 30;
 };
-
 }
 
 #endif // TSDB_HFCPP_HPP
